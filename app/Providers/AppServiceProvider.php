b2c24b10035134e90e05014252295032aa202d5a
guanyuming <264082251@qq.com>
2020-09-09 16:01:35 +0800

    用户个人页原型

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,15 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (env('APP_ENV') == 'local') {
+            DB::listen(function ($query)
+            {
+                $sql = str_replace(["?", "%Y-%m-%d", "%Y-%m"], ["'%s'", "?Y-?m-?d", "?Y-?m"], $query->sql);
+                $sql = vsprintf($sql, $query->bindings);
+                $sql = str_replace(["\\", "?Y-?m-?d", "?Y-?m"], ["", "%Y-%m-%d", "%Y-%m"], $sql);
+                Log::info($sql);
+            });
+        }
     }
 
     /**
