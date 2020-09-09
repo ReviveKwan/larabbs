b5913a8d220bbfd638c3a72e7aac076bb50783a9
guanyuming <264082251@qq.com>
2020-09-09 16:01:42 +0800

    用户个人页原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    //show 视图方法 由于 show() 方法传参时声明了类型 —— Eloquent 模型 User，对应的变量名 $user 会匹配路由片段中的 {user}，这样，Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例。
+    // 此功能称为 『隐性路由模型绑定』，是『约定优于配置』设计范式的体现，同时满足以下两种情况，此功能即会自动启用：
+    // 1). 路由声明时必须使用 Eloquent 模型的单数小写格式来作为 路由片段参数，User 对应 {user}：
+    // 2). 控制器方法传参中必须包含对应的 Eloquent 模型类型 提示，并且是有序的：
+    public function show(User $user)
+    {
+        //我们将用户对象变量 $user 通过 compact 方法转化为一个关联数组，并作为第二个参数传递给 view 方法，将变量数据传递到视图中。
+        return view('users.show', compact('user'));
+    }
+
+
+}
