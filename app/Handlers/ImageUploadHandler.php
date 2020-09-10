bfd29735f9f2b91ce7f5e124299bd73445a58588
guanyuming <264082251@qq.com>
2020-09-10 11:37:00 +0800

    授权访问

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Handlers;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
     protected $allow_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
         // 文件夹切割能让查找效率更高。
@@ -19,16 +20,41 @@ class ImageUploadHandler
         // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
         $extension = strtolower($file->getClientOriginalExtension()) ? :'png';
 
+        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
+        // 值如：1_1493521050_7BVc9v9ujP.png
         $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
 
+        // 如果上传的不是图片将终止操作
         if (!in_array($extension, $this->allow_ext)) {
             return false;
         }
-
+        //图片移动到目标存储路径中
         $file->move($upload_path, $filename);
+
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path.'/'.$filename, $max_width);
+        }
 
         return [
             'path' => config('app.url'). "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        //先实例化，传参是文件夹的磁盘物理地址
+        $image = Image::make($file_path);
+
+        //进行大小调整操作
+        $image->resize($max_width, null , function($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        //修改后保存
+        $image->save();
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['expect'=>['show']]);
+    }
+
     //show 视图方法 由于 show() 方法传参时声明了类型 —— Eloquent 模型 User，对应的变量名 $user 会匹配路由片段中的 {user}，这样，Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例。
     // 此功能称为 『隐性路由模型绑定』，是『约定优于配置』设计范式的体现，同时满足以下两种情况，此功能即会自动启用：
     // 1). 路由声明时必须使用 Eloquent 模型的单数小写格式来作为 路由片段参数，User 对应 {user}：
@@ -20,15 +25,17 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if($result) {
                 $data['avatar'] = $result['path'];
             }
