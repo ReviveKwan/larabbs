11d9155513d3d2fd5254c29189a6f54bffe04a7f
guanyuming <264082251@qq.com>
2020-09-10 16:09:29 +0800

    填充用户和话题数据库

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //生成数据集合
+        $users = factory(User::class)->times(10)->create();
+
+        //单独处理一个用户的数据
+        $user = User::query()->find(1);
+        $user->name = "ReviveKwan";
+        $user->email = "264082251@qq.com";
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
