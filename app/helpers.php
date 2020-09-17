40d13ce886fae9c59c7547e1963988093f74208b
guanyuming <264082251@qq.com>
2020-09-17 18:08:23 +0800

    用户创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
@@ -10,6 +11,12 @@ function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return \Illuminate\Support\Str::limit($excerpt, $length);
 }
 
 
